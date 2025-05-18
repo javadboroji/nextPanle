3e55bc8bcce213d44a8d6b82f677dbc3b86e7000
javad <javadboroji2222@gmail.com>
2025-05-18 10:32:22 +0330

    [fetch] : add loading to page

--- webapp/app/(panle)/panle/page.tsx
+++ webapp/app/(panle)/panle/page.tsx
@@ -4,12 +4,13 @@ import ModalLayout from '@/components/Modal/Modal'
 import React, { useState } from 'react'
 import Layout from './Layout'
 import Uploader from '@/components/Uploader/Uploader'
+import Loading from '@/components/Loading/Loading'
 
 function page() {
  const [images, setImages] = useState("")
   return (
     <Layout> 
-      test
+      <Loading/>
     </Layout>
   )
 }
