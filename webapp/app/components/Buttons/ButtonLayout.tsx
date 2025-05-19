a2120946868f93e01847f94e777b7766bd8fa456
javd boroji <javadboroji22222@gmail.com>
2025-05-19 21:15:29 +0330

    [fetch]:add ButtonLayout

---
+++ webapp/app/components/Buttons/ButtonLayout.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+interface ICbutton {
+  children: React.ReactNode;
+  btnType: "fill" | "outline" | "link";
+  type?: "submit" | "reset" | "button";
+  size?: "sm" | "md" | "lg";
+  onClick?: (data?: any) => void;
+}
+const ButtonLayout: React.FC<ICbutton> = ({
+  children,
+  btnType,
+  type,
+  size,
+  onClick,
+}) => {
+  const renderbtnTypeClassName = () => {
+    switch (btnType) {
+      case "fill":
+        return "bg-blue-500 text-white";
+      case "outline":
+        return "bg-white border-[1px] border-blue-500 text-blue-500";
+      case "link":
+        return "text-blue-500";
+      default:
+        return "";
+    }
+  };
+  const renderSizeClassName = () => {
+    switch (size) {
+      case "sm":
+        return "px-4 py-1 text-sm";
+      case "md":
+        return "px-6 py-2 text-base";
+      case "lg":
+        return "px-10 py-3 text-lg";
+      default:
+        return "px-4 py-1 text-base";
+    }
+  };
+  return (
+    <button
+      type={type}
+      onClick={(data) => {
+        if (onClick) onClick(data);
+      }}
+      className={`hover:cursor-pointer  rounded-xl m-2    ${renderbtnTypeClassName()} ${renderSizeClassName()}`}
+    >
+      {children}
+    </button>
+  );
+};
+export default ButtonLayout;
