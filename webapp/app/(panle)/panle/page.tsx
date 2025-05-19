1e22d6985f9b747f824ed9dc78b89cedbc112411
javad <javadboroji2222@gmail.com>
2025-05-19 09:16:51 +0330

    [fetch] :add button component

--- webapp/app/(panle)/panle/page.tsx
+++ webapp/app/(panle)/panle/page.tsx
@@ -6,13 +6,22 @@ import Layout from './Layout'
 import Uploader from '@/components/Uploader/Uploader'
 import Loading from '@/components/Loading/Loading'
 import { toast, Toaster } from "sonner"
+import Cbutton from '@/components/Button/Cbutton'
 function page() {
- const [images, setImages] = useState("")
+ const [images, setImages] = useState("");
+ const clickHandler=(data:any)=>{
+  console.log(data ,'*********');
+  
+ }
   return (
     <Layout> 
       <button className='' onClick={()=>toast("success")}> click Toast</button>
       {/* <Loading/> */}
-      
+      <div className='flex items-center'>
+      <Cbutton type='button'  btnType='fill' size='lg' onClick={()=>clickHandler({user:"dawdwad"})}> کلیک</Cbutton>
+      <Cbutton type='button'  btnType='link'> کلیک</Cbutton>
+      <Cbutton type='button'  btnType='outline'> کلیک</Cbutton>
+      </div>
     </Layout>
   )
 }
